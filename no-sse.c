f7677a7d07375e6bcbb019b20f9cd4f5c642525d
stellakokolaki <32705062+stellakokolaki@users.noreply.github.com>
2020-04-10 17:43:13 +0300

    Update no-sse.c

--- no-sse.c
+++ no-sse.c
@@ -1,7 +1,8 @@
+// compile with: gcc -Wall -O2 no-sse.c -o no-sse -DN=10000 -DM=10000
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-// compile with: gcc -Wall -O2 no-sse.c -o no-see -DN=10000 -DM=10000
+
 
 void get_walltime(double *wct){
 	struct timeval tp;
@@ -10,16 +11,17 @@ void get_walltime(double *wct){
 }
 
 int main(){
+
 	float *array01, *array02;
   	float k0 = 0.5, k1 = 0.5, k2 = 0.5, k3 = 0.5, k4 =5.0, k5 = 0.5, k6 = 0.5, k7 = 0.5, k8 = 0.5;
 	double ts, te, mflops;
 	int i, j;
 
- // allocate test arrays
- 	 array01 = (float*) malloc(N* M* sizeof(float));
-  	 array02= (float*) malloc(N* M* sizeof(float));
+// allocate test arrays
+	array01 = (float*) malloc(N* M* sizeof(float));
+  	array02 = (float*) malloc(N* M* sizeof(float));
 
- //initialize all arrays - cache warm-up
+//initialize all arrays - cache warm-up
 	for(i=0; i < N *M; i++){
 		array01[i] = ((float)rand()/(float)(RAND_MAX));
   		array02[i] = ((float)rand()/(float)(RAND_MAX));
@@ -27,27 +29,27 @@ int main(){
 	if (array01==NULL) exit(1);
 	if (array02==NULL) exit(1);
 
- //Starting time
+//Starting time
 	get_walltime(&ts);
-	printf("%f\n", ts);
+	printf("\nTime Start :%f\n", ts);
 
- //Calculations with temp 
+//Create table images
 	for(i = 1; i < N - 1; i++){
 		for(j = 1; j < M - 1; j++){
 			array02[j] = (array01[(i-1)+(j-1)] * k0) + (array01[(i-1)+j] * k1) + (array01[(i-1)+(j+1)] * k2) + (array01[i+(j-1)] * k3) + (array01[i+j] * k4) + (array01[i+(j+1)] * k5) + (array01[(i+1)+(j-1)] * k6) + (array01[(i+1)+j] * k7) + (array01[(i+1)+(j+1)] * k8);
 		}
 	}
- // get ending time  & print mflops/sec (2 operations per R*N passes)
-	get_walltime(&te);	
-	printf("%f\n", te);
-	printf("%f\n", te-ts);
+
+// compute mflops/sec (2 operations per R*N passes)
+	get_walltime(&te);		
+	printf("End Time:%f\n", te);
+	printf("Calculating time : %f\n", te-ts);
 
 	mflops = (2.0*M*N)/((te-ts)*1e6);
-  	printf("MFLOPS/sec = %f\n",mflops);
+ 	printf("MFLOPS/sec = %f\n\n",mflops);
 
- // free arrays
+// free arrays
 	free(array01);
   	free(array02);
-	
 	return 0;
 }
