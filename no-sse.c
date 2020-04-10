9bfda86d9e088207ebf6de023388828bbe22f9a8
stellakokolaki <32705062+stellakokolaki@users.noreply.github.com>
2020-04-10 16:42:01 +0300

    Update no-sse.c

--- no-sse.c
+++ no-sse.c
@@ -16,28 +16,26 @@ int main(){
 	double ts, te, mflops;
 	int i, j;
 
-// allocate test arrays
+ // allocate test arrays
  	 array01 = (float*) malloc(N* M* sizeof(float));
   	 array02= (float*) malloc(N* M* sizeof(float));
 
-//initialize all arrays - cache warm-up
+ //initialize all arrays - cache warm-up
 	for(i=0; i < N *M; i++){
 		array01[i] = ((float)rand()/(float)(RAND_MAX));
   	array02[i] = ((float)rand()/(float)(RAND_MAX));
 	}
-//Starting time
+ //Starting time
 	get_walltime(&ts);
 	printf("%f\n", ts);
 
-//Calculations with temp 
+ //Calculations with temp 
 	for(i = 1; i < N - 1; i++){
 		for(j = 1; j < M - 1; j++){
 			array02[j] = (array01[(i-1)+(j-1)] * k0) + (array01[(i-1)+j] * k1) + (array01[(i-1)+(j+1)] * k2) + (array01[i+(j-1)] * k3) + (array01[i+j] * k4) + (array01[i+(j+1)] * k5) + (array01[(i+1)+(j-1)] * k6) + (array01[(i+1)+j] * k7) + (array01[(i+1)+(j+1)] * k8);
 		}
 	}
-
-
-// get ending time  & print mflops/sec (2 operations per R*N passes)
+ // get ending time  & print mflops/sec (2 operations per R*N passes)
 	get_walltime(&te);	
 	printf("%f\n", te);
 	printf("%f\n", te-ts);
@@ -45,7 +43,7 @@ int main(){
 	mflops = (2.0*M*N)/((te-ts)*1e6);
   	printf("MFLOPS/sec = %f\n",mflops);
 
-// free arrays
+ // free arrays
 	free(array01);
   	free(array02);
 	
